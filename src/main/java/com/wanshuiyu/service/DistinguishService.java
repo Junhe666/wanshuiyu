6c89bb3f756f492b456f43df194b05e45e4be7c2
Junhe <devopscode@126.com>
2017-10-23 14:27:51 +0800

    识别

--- src/main/java/com/wanshuiyu/service/DistinguishService.java
+++ src/main/java/com/wanshuiyu/service/DistinguishService.java
@@ -8,7 +8,7 @@ import java.util.Map;
  * Created by Administrator on 2017/10/22.
  * 规则
  */
-public class RuleService {
+public class DistinguishService {
 
     //水鱼
     public boolean king(Map<Integer,Poker> pokers){
@@ -32,4 +32,23 @@ public class RuleService {
         }
         return false;
     }
+
+    //麻麻
+    public boolean zero(Map<Integer,Poker> pokers){
+        //判断 10
+        if(pokers.get(1).getPoint() ==10 && pokers.get(1).getPoint()==10 &&
+                (pokers.get(3).getPoint()+pokers.get(4).getPoint())==10){
+            return true;
+        }
+        //判定 大相加10 小相加10
+        if ((pokers.get(1).getPoint() + pokers.get(2).getPoint()==10)&&(pokers.get(3).getPoint() + pokers.get(4).getPoint()== 10)){
+            return true;
+        }
+//        //判断 10 11 12 13
+//        if((pokers.get(1).getPoint() >= 10 && pokers.get(1).getPoint()<=13) &&){
+//
+//        }
+        return false;
+    }
+
 }
